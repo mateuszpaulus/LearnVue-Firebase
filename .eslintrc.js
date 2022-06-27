28ca35e2ca5f872528102223cfb5c07bb351d309
mateuszpaulus <paulus.m.mateusz@gmail.com>
2022-06-27 22:03:00 +0200

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: ['plugin:vue/recommended', '@vue/airbnb'],
+  parserOptions: {
+    parser: '@babel/eslint-parser',
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['html'],
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'singleline-html-element-content-newline': 'off',
+    'vue/multiline-html-element-content-newline': 'off',
+    'max-len': 0,
+    'linebreak-style': 0,
+    'template-curly-spacing': 'off',
+    indent: [
+      'error',
+      2,
+      {
+        ignoredNodes: ['TemplateLiteral'],
+      },
+    ],
+    'no-unused-vars': 'off',
+  },
+};
---
+++ src/firebase/firebaseInit.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDtF5M0NSafmaTgAxuL2HRYkQDLY3Z-Gyg',
+  authDomain: 'learnvue-firebase.firebaseapp.com',
+  projectId: 'learnvue-firebase',
+  storageBucket: 'learnvue-firebase.appspot.com',
+  messagingSenderId: '558828491015',
+  appId: '1:558828491015:web:6e077efe2f5f06b5a6b5a2',
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const timestamp = firebase.firestore.FieldValue.serverTimestamp;
+
+export { timestamp };
+export default firebaseApp.firestore();
