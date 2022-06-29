444323109b55e652289a75cacf7dcf0b7dd1530a
mateuszpaulus <paulus.m.mateusz@gmail.com>
2022-06-30 01:40:55 +0200

    change import firebase

--- src/firebase/firebaseInit.js
+++ src/firebase/firebaseInit.js
@@ -1,5 +1,5 @@
-import firebase from 'firebase/app';
-import 'firebase/firestore';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyDtF5M0NSafmaTgAxuL2HRYkQDLY3Z-Gyg',
