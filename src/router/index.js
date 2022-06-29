9a50218ef6175dc9f017fef0f5dc546a96304f3a
mateuszpaulus <paulus.m.mateusz@gmail.com>
2022-06-29 02:52:07 +0200

    add display document title

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,10 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import WelcomeView from '@/views/Welcome.vue';
+import PostsView from '@/views/Posts.vue';
+import LoginView from '@/views/Login.vue';
+import RegisterView from '@/views/Register.vue';
+import ForgotPasswordView from '@/views/ForgotPassword.vue';
 
 Vue.use(VueRouter);
 
@@ -9,6 +13,41 @@ const routes = [
     path: '/',
     name: 'WelcomeView',
     component: WelcomeView,
+    meta: {
+      title: 'Welcome',
+    },
+  },
+  {
+    path: '/posts',
+    name: 'PostsView',
+    component: PostsView,
+    meta: {
+      title: 'Posts',
+    },
+  },
+  {
+    path: '/login',
+    name: 'LoginView',
+    component: LoginView,
+    meta: {
+      title: 'Login',
+    },
+  },
+  {
+    path: '/register',
+    name: 'RegisterView',
+    component: RegisterView,
+    meta: {
+      title: 'Register',
+    },
+  },
+  {
+    path: '/forgot-password',
+    name: 'ForgotPasswordView',
+    component: ForgotPasswordView,
+    meta: {
+      title: 'Forgot Password',
+    },
   },
 ];
 
@@ -16,6 +55,11 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  document.title = `${to.meta.title} | Learn Vue`;
+  next();
 });
 
 export default router;
