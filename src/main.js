f72cb27ce3e7afceb672ce13dee7bf18cc3736ce
mateuszpaulus <paulus.m.mateusz@gmail.com>
2022-06-30 01:33:01 +0200

    add auth state change when app is not initialized

--- src/main.js
+++ src/main.js
@@ -1,14 +1,21 @@
 import Vue from 'vue';
+import firebase from 'firebase/compat/app';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 import vuetify from './plugins/vuetify';
+import 'firebase/compat/auth';
 
 Vue.config.productionTip = false;
 
-new Vue({
-  router,
-  store,
-  vuetify,
-  render: (h) => h(App),
-}).$mount('#app');
+let app;
+firebase.auth().onAuthStateChanged(() => {
+  if (!app) {
+    new Vue({
+      router,
+      store,
+      vuetify,
+      render: (h) => h(App),
+    }).$mount('#app');
+  }
+});
