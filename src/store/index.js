785bbe85ec6172e89c9044ebf7a09c67e2528925
mateuszpaulus <paulus.m.mateusz@gmail.com>
2022-06-29 02:53:25 +0200

    add store

--- src/store/index.js
+++ src/store/index.js
@@ -5,13 +5,20 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    samplePostCards: [
+      { postTitle: 'Post Card #1', postCoverPhoto: 'stock-1', postDate: 'June 28, 2022' },
+      { postTitle: 'Post Card #2', postCoverPhoto: 'stock-2', postDate: 'June 28, 2022' },
+      { postTitle: 'Post Card #3', postCoverPhoto: 'stock-3', postDate: 'June 28, 2022' },
+      { postTitle: 'Post Card #4', postCoverPhoto: 'stock-4', postDate: 'June 28, 2022' },
+    ],
+    editPost: null,
   },
-  getters: {
-  },
+  getters: {},
   mutations: {
+    toggleEditPost(state, payload) {
+      state.editPost = payload;
+    },
   },
-  actions: {
-  },
-  modules: {
-  },
+  actions: {},
+  modules: {},
 });
