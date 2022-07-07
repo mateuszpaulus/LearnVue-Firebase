d6b6caee13c28e9c90f548f8600f322ef3771f9e
mateuszpaulus <paulus.m.mateusz@gmail.com>
2022-07-07 13:39:57 +0200

    add preview post

---
+++ src/views/PreviewPost.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="post-view">
+    <div class="container quillWrapper">
+      <h2>{{ postTitle }}</h2>
+      <img :src="postCoverPhoto" alt="" />
+      <div class="post-content ql-editor">{{ postHTML }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PreviewPostView',
+  computed: {
+    postTitle() {
+      return this.$store.state.postTitle;
+    },
+    postHTML() {
+      return this.$store.state.postHTML;
+    },
+    postCoverPhoto() {
+      return this.$store.state.postPhotoFileURL;
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.post-view {
+  min-height: 100%;
+
+  .container {
+    max-width: 1000px;
+    padding: 60px 25px;
+  }
+
+  .ql-editor {
+    padding: 0;
+  }
+
+  h2 {
+    margin-bottom: 16px;
+    font-weight: 300;
+    font-size: 32px;
+  }
+
+  img {
+    width: 100%;
+    margin-bottom: 32px;
+  }
+}
+</style>
